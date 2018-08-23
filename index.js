7f04edcd15eda7c8f7818233886df6a44494afa8
Zach VanSomeren <zvansomeren@gmail.com>
2018-08-23 11:25:44 -0700

    'Basic express app done'

---
+++ index.js
@@ -0,0 +1,19 @@
+const bodyParser = require('body-parser');
+const ejsLayouts = require('express-ejs-layouts');
+const express = require('express');
+const passport = require('passport');
+const session = require('express-session');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(ejsLayouts);
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+
+});
+
+app.listen(3000, () => {
+  console.log('You\'re listening to the smooth sounds of port 3000 in the morning');
+});
